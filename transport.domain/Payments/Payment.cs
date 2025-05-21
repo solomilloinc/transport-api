e1ffede611a56d116bb9602ef67498c4e3f7c081
x <x@APILOT-XNGG3XL2>
2025-05-21 08:25:00 -0300

    Refactor Payment class for auditing and tracking
    - Updated using directives for shared kernel.
    - Payment class now inherits from Entity and implements IAuditable.
    - Added CreatedBy and UpdatedBy properties for tracking user actions.
    - Introduced CreatedDate and UpdatedDate properties for timestamping.

--- transport.domain/Payments/Payment.cs
+++ transport.domain/Payments/Payment.cs
@@ -1,9 +1,8 @@
-﻿using Transport.Domain.Customers;
-using Transport.SharedKernel.Contracts.Payment;
+﻿using Transport.SharedKernel;
 
 namespace Transport.Domain.Payments;
 
-public class Payment
+public class Payment: Entity, IAuditable
 {
     public int PaymentId { get; set; }
     public long? PaymentMpId { get; set; }
@@ -12,4 +11,9 @@ public class Payment
     public string Status { get; set; }
     public DateTime? DateApproved { get; set; }
     public string RawJson { get; set; } = null!;
+
+    public string CreatedBy { get; set; } = null!;
+    public string? UpdatedBy { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedDate { get; set; }
 }
